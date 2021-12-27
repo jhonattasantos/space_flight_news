10711de26c7e24c7023486b6d183ee5bcb6b78fe
jhonatta <jhonattasantoss@hotmail.com>
2021-12-27 11:33:02 -0300

    test: unit tests article model

--- space-rails-api/spec/models/article_spec.rb
+++ space-rails-api/spec/models/article_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:valid_attributes) {
+    {
+      featured: Faker::Boolean.boolean,
+      title: Faker::ChuckNorris.fact,
+      url: Faker::Internet.url,
+      imageUrl: Faker::Avatar.image,
+      newsSite: Faker::Internet.url,
+      summary: Faker::Lorem.paragraph,
+      publishedAt: Faker::Date.between(from: 2.year.ago, to: Date.today)
+    }
+  }
+
+  let(:invalid_attributes) {
+    {
+      featured: Faker::Boolean.boolean,
+      title: nil,
+      url: Faker::Internet.url,
+      imageUrl: Faker::Avatar.image,
+      newsSite: Faker::Internet.url,
+      summary: Faker::Lorem.paragraph,
+      publishedAt: Faker::Date.between(from: 2.year.ago, to: Date.today)
+    }
+  }
+
+  it "should article instance is valid" do
+    article = Article.new(valid_attributes)
+    expect(article).to be_valid
+  end
+
+  it "should article instance is invalid" do
+    article = Article.new(title: nil)
+    article.featured = Faker::Boolean.boolean
+    article.url = Faker::Internet.url 
+    article.valid?
+    expect(article.errors[:title]).to include("can't be blank")
+  end
+
 end
